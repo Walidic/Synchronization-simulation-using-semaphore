f5dc8731170b4644d739e9be8c17b217d83ac3a6
Adham <adhamahmad541@gmail.com>
2021-12-04 22:43:22 +0200

    router draft

--- src/Router.java
+++ src/Router.java
@@ -1,3 +1,16 @@
-public class Router {
-    
+import java.util.List;
+import java.util.ArrayList
+public class Router 
+{
+    int numOfConnections; // max num of connections allowed
+    int currentConnections = 0;
+    List<Device> connectedDevices = new ArrayList<>();
+
+    public void occupyConnection (Device d) {
+        connectedDevices.add(d);
+    }
+    public void releaseConnection (Device d) {
+        connectedDevices.remove(d);
+    }
+
 }
