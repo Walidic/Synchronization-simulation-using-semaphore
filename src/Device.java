1b9c015a5411712c2ca980ca48e101b0d3ddb9cd
Walidic <OMAR.walid123456@gmail.com>
2021-12-04 22:13:51 +0200

    Initial Device class

--- src/Device.java
+++ src/Device.java
@@ -1,3 +1,24 @@
-public class Device {
+public class Device extends Thread{
+    private String name;
+    private String type;
 
+    public void setDeviceName(String inputName){
+        name  = inputName;
+    }
+
+    public void SetDeviceType(String inputType){
+        type = inputType;
+    }
+
+    public String getDeviveName(){
+        return name;
+    }
+
+    public String getDeviceType(){
+        return type;
+    }
+
+    public void connect (){
+
+    }
 }
