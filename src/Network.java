b80172b163066950db8b2455977927fbf062bb8b
Walidic <74151292+Walidic@users.noreply.github.com>
2021-12-12 22:11:21 +0200

    Removed GUI stuff

--- src/Network.java
+++ src/Network.java
@@ -1,43 +1,43 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+// import javafx.application.Application;
+// import javafx.event.ActionEvent;
+// import javafx.event.EventHandler;
+// import javafx.fxml.FXMLLoader;
+// import javafx.scene.Parent;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.layout.StackPane;
+// import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-public class Network extends Application {
+public class Network{
     static public int maxNumOfConnections;
     static public int numOfDevices;
 
-    @Override
-    public void start(Stage stage) {
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
-            Scene scene = new Scene(root);
-            stage.setTitle("Java Synchronization");
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    // @Override
+    // public void start(Stage stage) {
+    //     Parent root;
+    //     try {
+    //         root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+    //         Scene scene = new Scene(root);
+    //         stage.setTitle("Java Synchronization");
+    //         stage.setScene(scene);
+    //         stage.show();
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        // System.out.println("What is the number of WI-FI Connections?");
-        // maxNumOfConnections = sc.nextInt();
-        // System.out.println("What is the number of devices Clients want to connect?");
-        // numOfDevices = sc.nextInt();
+        System.out.println("What is the number of WI-FI Connections?");
+        maxNumOfConnections = sc.nextInt();
+        System.out.println("What is the number of devices Clients want to connect?");
+        numOfDevices = sc.nextInt();
 
-        launch(args);
+        // launch(args);
         Device devices[] = new Device[numOfDevices];
         Router router = new Router(maxNumOfConnections);
 
--- src/mainWindowController.java
+++ src/mainWindowController.java
@@ -1,26 +1,26 @@
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+// import javafx.event.ActionEvent;
+// import javafx.fxml.FXML;
+// import javafx.scene.control.Button;
+// import javafx.scene.control.TextField;
+// import javafx.stage.Stage;
 
-public class mainWindowController {
+// public class mainWindowController {
 
-    @FXML
-    private Button Start;
+//     @FXML
+//     private Button Start;
 
-    @FXML
-    private TextField WIFINo;
+//     @FXML
+//     private TextField WIFINo;
 
-    @FXML
-    private TextField deviceNo;
+//     @FXML
+//     private TextField deviceNo;
 
-    @FXML
-    void exit(ActionEvent event) {
-        Stage window = (Stage) deviceNo.getScene().getWindow();
-        Network.maxNumOfConnections = Integer.parseInt(WIFINo.getText());
-        Network.numOfDevices = Integer.parseInt(deviceNo.getText());
-        window.close();
-    }
+//     @FXML
+//     void exit(ActionEvent event) {
+//         Stage window = (Stage) deviceNo.getScene().getWindow();
+//         Network.maxNumOfConnections = Integer.parseInt(WIFINo.getText());
+//         Network.numOfDevices = Integer.parseInt(deviceNo.getText());
+//         window.close();
+//     }
 
-}
+// }
